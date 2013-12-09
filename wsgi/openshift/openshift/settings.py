ef838f6493a9fb5bac53be0637c9c9cdaa365ef9
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2013-12-09 15:27:14 -0200

    fix

--- wsgi/openshift/openshift/settings.py
+++ wsgi/openshift/openshift/settings.py
@@ -2,7 +2,7 @@
 import imp, os
 
 # a setting to determine whether we are running on OpenShift
-ON_OPENSHIFT = os.environ.has_key('OPENSHIFT_REPO_DIR'):
+ON_OPENSHIFT = os.environ.has_key('OPENSHIFT_REPO_DIR')
 
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
